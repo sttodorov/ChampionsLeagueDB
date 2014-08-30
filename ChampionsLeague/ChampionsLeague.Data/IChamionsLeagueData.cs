ef18c6c4b25aa0562e136badf65df18cc4efe6b3
sttodorov <st.todorov9@gmail.com>
2014-08-30 13:46:29 +0300

    Repository pattern

---
+++ ChampionsLeague/ChampionsLeague.Data/IChamionsLeagueData.cs
@@ -0,0 +1,19 @@
+﻿namespace ChampionsLeague.Data
+{
+    using ChampionsLeague.Data.Repositories;
+    using ChampionsLeague.Model;
+
+    public interface IChamionsLeagueData
+    {
+        IGenericRepository<Town> Towns {get;}
+
+        IGenericRepository<Stadium> Stadiums { get; }
+
+        IGenericRepository<Team> Teams { get; }
+
+        IGenericRepository<Player> Players { get; }
+
+        IGenericRepository<Match> Matches { get; }
+
+    }
+}
---
+++ ChampionsLeague/ChampionsLeague.Data/IChampionsLeagueContext.cs
@@ -0,0 +1,27 @@
+﻿namespace ChampionsLeague.Data
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+    using ChampionsLeague.Model;
+
+    public interface IChampionsLeagueContext
+    {
+        IDbSet<Town> Towns { get; set; }
+
+        IDbSet<Stadium> Stadiums { get; set; }
+        
+        IDbSet<Team> Teams { get; set; }
+        
+        IDbSet<Player> Players { get; set; }
+        
+        IDbSet<Match> Matches { get; set; }
+
+        IDbSet<T> Set<T>() where T : class;
+
+        DbEntityEntry<T> Entry<T>(T entity) where T : class;
+
+        int SaveChanges();
+    }
+}
