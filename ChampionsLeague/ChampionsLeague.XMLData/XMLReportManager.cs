4ed47eaad502f773db0b5abc2143574660117579
tsigularoff <tsigularoff@gmail.com>
2014-08-30 23:47:15 +0300

    xml parser fixes

---
+++ ChampionsLeague/ChampionsLeague.XMLData/XMLReportManager.cs
@@ -0,0 +1,59 @@
+﻿namespace ChampionsLeague.XMLData
+{
+    using System;    
+    using System.Linq;
+    
+    using ChampionsLeague.Data;    
+    using ChampionsLeague.Model;
+    using System.Data.Entity.Validation;
+
+    public class XMLReportManager
+    {
+        XMLParser parser;
+
+        public XMLReportManager()
+        {
+            this.parser = new XMLParser();
+        }
+
+        public void LoadMatchReportsInDb(string filename)
+        {            
+            var matches = this.parser.LoadMatchReport(filename);
+
+            using(var db = new ChampionsLeagueContext())
+            {
+                foreach (var match in matches)
+                {
+                    db.Matches.Add(match);
+                }
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch(DbEntityValidationException e)
+                {
+                    Console.WriteLine(e.Message);
+
+                    foreach (var item in e.EntityValidationErrors)
+                    {
+                        foreach (var err in item.ValidationErrors)
+                        {
+                            Console.WriteLine(err.ErrorMessage);
+                        }
+                    }
+                }
+
+                
+            }            
+        }
+
+        public void SaveMatchReportsFromDb(string filename)
+        { 
+            using(var db = new ChampionsLeagueContext())
+            {
+                var matches = db.Matches;
+                this.parser.SaveMatchReport(filename, matches.ToList());                
+            }
+        }
+    }
+}
