45186583c3e1932444f9c8e8b0f408bfb4b00369
tsigularoff <tsigularoff@gmail.com>
2014-08-30 16:43:22 +0300

    XML Parser -> SaveMatchReport (ICollection<Match> -> save to file), LoadMatchReport(load from file -> return ICollection<Match>)

---
+++ ChampionsLeague/ChampionsLeague.XMLParserTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ChampionsLeague.XMLDataParser;
+using ChampionsLeague.Model;
+using ChampionsLeague.Data;
+
+
+namespace ChampionsLeague.XMLParserTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+
+            string path = @"..\..\matchReport.xml";
+
+            var parser = new XMLParser();                        
+
+            using(var db = new ChampionsLeagueContext())
+            {
+                var matches = db.Matches.ToList();                
+                parser.SaveMatchReport(path, matches);
+            }
+           
+
+            var m = parser.LoadMatchReport(path);
+            
+            foreach (var item in m)
+            {
+                Console.WriteLine(item.HostTeamId);
+            }
+        }
+    }
+}
