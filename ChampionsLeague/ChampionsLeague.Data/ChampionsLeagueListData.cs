cc214142e29c81bf6e7e422cc33c47879a429251
sttodorov <st.todorov9@gmail.com>
2014-09-03 15:49:26 +0300

    Added UnitTest project

---
+++ ChampionsLeague/ChampionsLeague.Data/ChampionsLeagueListData.cs
@@ -0,0 +1,25 @@
+﻿namespace ChampionsLeague.Data
+{
+    using ChampionsLeague.Model;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ChampionsLeagueListData
+    {
+        public List<Town> Towns { get; set; }
+        public List<Stadium> Stadiums { get; set; }
+        public List<Team> Teams { get; set; }
+        public List<Player> Players { get; set; }
+        public List<Match> Matches { get; set; }
+
+        public ChampionsLeagueListData()
+        {
+            this.Towns = new List<Town>();
+            this.Stadiums = new List<Stadium>();
+            this.Teams = new List<Team>();
+            this.Players = new List<Player>();
+            this.Matches = new List<Match>();
+        }
+    }
+}
---
+++ ChampionsLeague/ChampionsLeague.Tests/MSSQLDatabase.cs
@@ -0,0 +1,80 @@
+﻿namespace ChampionsLeague.Tests
+{
+    using System;
+    using System.Linq;
+    
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    
+    using ChampionsLeague.Data;
+    using ChampionsLeague.Model;
+
+    [TestClass]
+    public class MSSQLDatabase
+    {
+        private ChampionsLeagueListData testDb;
+        
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.testDb = new ChampionsLeagueListData();
+        }
+
+        [TestMethod]
+        public void ShouldCreateEmptyListsOnInitialize()
+        {
+            Assert.AreEqual(this.testDb.Towns.Count, 0);
+            Assert.AreEqual(this.testDb.Teams.Count, 0);
+            Assert.AreEqual(this.testDb.Stadiums.Count, 0);
+            Assert.AreEqual(this.testDb.Players.Count, 0);
+            Assert.AreEqual(this.testDb.Matches.Count, 0);
+        }
+
+        [TestMethod]
+        public void ShouldAddSingleObject()
+        {
+            var townToAdd = new Town() { TownName = "TestTownCreating" };
+            var stadiumToAdd = new Stadium(){Name = "TestStadiumName", Town = townToAdd};
+            var teamToAdd = new Team(){TeamName = "TestTeamName", Town = townToAdd};
+            var playerToAdd = new Player(){FirstName="TestFirstName", LastName = "TestLastName", Team = teamToAdd, Salary = 2000};
+
+            this.testDb.Towns.Add(townToAdd);
+            this.testDb.Stadiums.Add(stadiumToAdd);
+            this.testDb.Teams.Add(teamToAdd);
+            this.testDb.Players.Add(playerToAdd);
+
+            Assert.AreEqual(this.testDb.Towns.Count, 1);
+            Assert.AreEqual(this.testDb.Teams.Count, 1);
+            Assert.AreEqual(this.testDb.Stadiums.Count, 1);
+            Assert.AreEqual(this.testDb.Players.Count, 1);
+        }
+
+        [TestMethod]
+        public void ShouldAddSingleMatchBetwenGivenTeams()
+        {
+            var townToAdd = new Town() { TownName = "TestTownCreating" };
+            var stadiumToAdd = new Stadium() { Name = "TestStadiumName", Town = townToAdd };
+            var hostTeamToAdd = new Team() { TeamName = "HOSTTeamName", Town = townToAdd };
+            var hostPlayerToAdd = new Player() { FirstName = "HOSTFirstName", LastName = "HOSTLastName", Team = hostTeamToAdd, Salary = 2000 };
+
+            this.testDb.Towns.Add(townToAdd);
+            this.testDb.Stadiums.Add(stadiumToAdd);
+            this.testDb.Teams.Add(hostTeamToAdd);
+            this.testDb.Players.Add(hostPlayerToAdd);
+
+            var guestTeamToAdd = new Team() { TeamName = "GUESTTeamName", Town = townToAdd };
+            var guestPlayerToAdd = new Player() { FirstName = "GUESTFirstName", LastName = "GUESTLastName", Team = guestTeamToAdd, Salary = 2000 };
+
+            this.testDb.Teams.Add(guestTeamToAdd);
+            this.testDb.Players.Add(guestPlayerToAdd);
+
+            this.testDb.Matches.Add(new Match() { HostTeam = hostTeamToAdd, GuestTeam = guestTeamToAdd, Stadium = stadiumToAdd, Date = DateTime.Now, Attendance = 12000 });
+
+            Assert.AreEqual(this.testDb.Matches.Count, 1);
+
+            var firstMatch = this.testDb.Matches.FirstOrDefault();
+            Assert.AreEqual(firstMatch.HostTeam.TeamName, hostTeamToAdd.TeamName);
+            Assert.AreEqual(firstMatch.GuestTeam.TeamName, guestTeamToAdd.TeamName);
+
+        }
+    }
+}
